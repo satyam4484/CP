f6492bfebe0e8799222bf5e38329c524cf5dd85c
satyam <satyambsingh93@gmail.com>
2021-09-13 08:39:49 +0530

    f1

---
+++ Dynamic programming/houserobber.cpp
@@ -0,0 +1,79 @@
+//SATYAM SINGH
+// problem link --> https://leetcode.com/problems/house-robber/
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll                 long long int
+#define pb                 push_back
+#define mod                     1000000007
+#define inf                     1e18
+#define ump                unordered_map
+#define mp                 make_pair
+#define nline                 "\n"
+#define all(v)                   v.begin(),v.end()
+
+#define  inpv(v,n)  for(int i=0;i<n;i++) cin>>v[i]
+#define  outv(v)    for(auto i:v) cout<<i<<' '
+#define  loop(i,a,b)  for(int i=(a);i<(b);i++)
+#define  looprev(i,a,b)  for(int i=(a);i>=0;i--)
+#define log(args...)  {string _s = #args ;replace(_s.begin(),_s.end(),',',' ') ; stringstream _ss(_s); istream_iterator<string> _it(_ss) ; err(_it,args);}
+void err(istream_iterator<string> it) {}
+template<typename T, typename ... Args>
+void err(istream_iterator<string> it, T a, Args... args) {
+    cout << *it << " = " << a << endl;
+    err(++it, args...);
+}
+
+void file_i_o()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("error.txt", "w", stderr);
+    freopen("output.txt", "w", stdout);
+#endif
+}
+
+// =============== CODE STARTS HERE DO NOT BOTHER ABOUT THE TEMPLATE IT"S FOR SIMPLICITY ==============================================================================================================================================
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+//         base cases
+        if (nums.size() == 1) return nums[0];
+        else if (nums.size() == 2) return max(nums[0], nums[1]);
+
+        vector<int>dp(nums.size());
+        dp[0] = nums[0], dp[1] = max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.size(); i++) {
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        return dp[nums.size() - 1];
+
+    }
+};
+
+void Main() {
+
+
+}
+
+
+// ============================================================================================================================
+int main( ) {
+    clock_t begin = clock();
+    file_i_o();
+    // Write your code here....
+
+    Main();
+
+    // code ends here !!!!!!
+#ifndef ONLINE_JUDGE
+    clock_t end = clock();
+    cout << "\n\nExecuted In: " << double(end - begin) / CLOCKS_PER_SEC * 100 << " seconds";
+#endif
+    return 0;
+}
