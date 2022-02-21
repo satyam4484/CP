3883d05c4736a7a4b6f32bafa1a61c6d9a22ddff
satyam singh <satyambsingh93@gmail.com>
2022-02-21 19:38:03 +0530

    21feb

---
+++ codeforces/queueAtSchool.cpp
@@ -0,0 +1,54 @@
+// problem link --> https://codeforces.com/problemset/problem/266/B
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll                 long long int
+#define pb                 push_back
+#define mod             1000000007
+#define inf             1e18
+#define ump                unordered_map
+#define mp                 make_pair
+
+#define  inpv(v,n)  for(int i=0;i<n;i++) cin>>v[i]
+#define  outv(v)    for(auto i:v) cout<<i<<' ' 
+#define  loop(i,a,b)  for(int i=(a);i<(b);i++)
+#define  looprev(i,a,b)  for(int i=(a);i>=0;i--)
+
+void file_i_o()
+{
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+void Main() {
+    int n,x;
+    cin>>n>>x;
+    string s;
+    cin>>s;
+    while(x--) {
+        for(int i=0;i<n-1;i++){
+            if(s[i] == 'B' and s[i+1] =='G') {
+                swap(s[i],s[i+1]);
+                i++;
+            }
+        }
+    }
+    cout<<s<<endl;
+}
+
+int main( ) {
+    clock_t begin = clock();
+    file_i_o();
+    // Write your code here....
+    Main();
+
+    // code ends here !!!!!! 
+    #ifndef ONLINE_JUDGE 
+      clock_t end = clock();
+      cout<<"\n\nExecuted In: "<<double(end - begin) / CLOCKS_PER_SEC<<" seconds";
+    #endif 
+    return 0;
+}
